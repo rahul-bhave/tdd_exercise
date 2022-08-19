8de7036006cbc856416f03f794ae11859e1ea068
rahulbhave <rsbhave81@gmail.com>
2022-08-19 12:12:36 +0530

    added test cases for checking exceptions

--- tests/unit/test_batch_process.py
+++ tests/unit/test_batch_process.py
@@ -4,6 +4,7 @@ This test is used to calculate available quanity after allocating the quanity to
 """
 from datetime import date
 from src.allocation.domain.model import Batch, OrderLine
+import pytest
 
 def test_allocating_to_a_batch_reduces_the_available_quanity():
     """
@@ -13,3 +14,21 @@ def test_allocating_to_a_batch_reduces_the_available_quanity():
     line = OrderLine('order-ref','SMALL-TABLE', 2)
     batch.allocate(line)
     assert batch.available_quanity == 18
+
+def test_to_check_if_allocating_more_than_the_available_quanity_raises_an_error():
+    """
+    This test is used to check if allocating more than the available quanity raises an error.
+    """
+    batch = Batch("batch-001","SMALL-TABLE",qty=20,eta=date.today())
+    line = OrderLine('order-ref','SMALL-TABLE', 22)
+    with pytest.raises(ValueError):
+        batch.allocate(line)
+
+def test_to_check_if_allocating_to_a_batch_with_different_sku_raises_an_error():
+    """
+    This test is used to check if allocating to a batch with different sku raises an error.
+    """
+    batch = Batch("batch-001","SMALL-TABLE",qty=20,eta=date.today())
+    line = OrderLine('order-ref','SMALL-TABLE-2', 2)
+    with pytest.raises(ValueError):
+        batch.allocate(line)
